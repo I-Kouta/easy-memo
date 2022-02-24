e1b2e51db951a7bcaae81e4a834037004bcff60b
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-24 16:12:10 +0900

    コントローラーとバリデーションの記述

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,8 +3,20 @@ class MemosController < ApplicationController
   end
 
   def new
+    @memo = Memo.new
   end
 
   def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redurect_to root_path
+    else
+      render:new
+    end
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:title_history, :why_content, :who_content, :what_content, :where_content, :content).merge(user_id: current_user.id)
   end
 end
