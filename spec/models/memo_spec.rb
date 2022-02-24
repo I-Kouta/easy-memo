7ea34e28c8f2dfe24c5575c8dc6a8e05162bfec9
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-24 19:39:17 +0900

    正常系テストの実行

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -8,12 +8,19 @@ RSpec.describe Memo, type: :model do
   describe 'メモ投稿' do
     context 'メモを投稿できる' do
       it '必須項目が全て記入されていれば投稿ができる' do
+        expect(@memo).to be_valid
       end
       it 'why_contentが空でも投稿できる' do
+        @memo.why_content = ''
+        expect(@memo).to be_valid
       end
       it 'who_contentが空でも投稿できる' do
+        @memo.who_content = ''
+        expect(@memo).to be_valid
       end
       it 'where_contentが空でも投稿できる' do
+        @memo.where_content = ''
+        expect(@memo).to be_valid
       end
     end
     context 'メモを投稿できない' do
