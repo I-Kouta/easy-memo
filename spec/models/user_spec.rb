fbb6dd4ea4855d6ede91732bbf3c8d423e4ddd3e
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-24 13:04:37 +0900

    rubocopの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe User, type: :model do
       it 'emailが@を含んでいない' do
         @user.email = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'passwordが入力されていない' do
         @user.password = ''
