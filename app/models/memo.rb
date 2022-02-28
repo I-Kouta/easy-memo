ee18f4cf4ef6e6def7e2bdf545d9194be6c983e5
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-28 22:34:44 +0900

    Memoモデルへの検索のメソッドの定義

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,4 +2,12 @@ class Memo < ApplicationRecord
   belongs_to :user
 
   validates :title_history, :content, presence: true
+
+  def self.search(search)
+    if search != ""
+      Mwmo.where('title_history LIKE(?)', 'why_content LIKE(?)', 'who_content LIKE(?)','where_content LIKE(?)','content LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      Memo.all
+    end
+  end
 end
