6ff85828928a848e6bca18515008073b37257eaf
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-02 14:10:25 +0900

    登録ができる際のテストコードの編集

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,13 +8,27 @@ RSpec.describe "ユーザー新規登録", type: :system do
   context '新規登録ができるとき' do
     it '正しい情報を入力すると新規登録ができてトップページに遷移する' do
       #トップページに遷移
+      visit root_path
       #トップページに新規登録ボタンがあることを確認
+      expect(page).to have_content("新規登録")
       #新規登録ページに遷移
+      visit new_user_registration_path
       #ユーザー情報を入力
+      fill_in 'Nickname', with: @user.nickname
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password_confirmation
       #サインアップボタンを押すとユーザーモデルのカウントが1上がる
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
       #トップページに遷移したことを確認
+      expect(current_path).to eq(root_path)
       #ログアウトボタンが表示されていることを確認
+      expect(page).to have_content('ログアウト')
       #ログイン・新規登録ボタンが表示されていないことを確認
+      expect(page).to have_no_content('ログイン')
+      expect(page).to have_no_content('新規登録')
     end
   end
   context '新規登録ができないとき' do
