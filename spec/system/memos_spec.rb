136a8aabc848a1636179125d2ab2c3f9a6e6dcec
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-02 20:07:20 +0900

    メモが投稿できる際のテスト実行

--- spec/system/memos_spec.rb
+++ spec/system/memos_spec.rb
@@ -9,19 +9,33 @@ end
 RSpec.describe "メモ投稿", type: :system do
   before do
     @user = FactoryBot.create(:user)
+    @memo_title_history = Faker::Lorem.sentence
+    @memo_content = Faker::Lorem.sentence
   end
   
   context 'メモが投稿できる' do
     it 'ログインしたユーザーはメモを投稿できる' do
       # basic認証の実行
+      basic_pass
       # ログインする
+      sign_in(@user)
       # トップページに遷移していることを確認
+      expect(current_path).to eq(root_path)
       # メモ投稿へのボタンがあることを確認
+      expect(page).to have_content('メモを投稿しましょう')
       # メモ投稿ページへ移動
+      visit new_memo_path
       # 内容を入力
+      fill_in 'タイトル(必須)', with: @memo_title_history
+      fill_in 'メモ内容(必須)', with: @memo_content
       # Memoモデルのカウントが1上がることを確認
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Memo.count }.by(1)
       # トップページに遷移することを確認
+      expect(current_path).to eq(root_path)
       # トップページに投稿したメモが存在していることを確認
+      expect(page).to have_content(@memo_title_history)
     end
   end
 
@@ -35,6 +49,4 @@ RSpec.describe "メモ投稿", type: :system do
       expect(page).to have_no_content('メモを投稿しましょう')
     end
   end
-
-
 end
