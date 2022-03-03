236296fd57bb35203ccba91973d2b06fd8e4601f
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-03 22:40:53 +0900

    Userモデルの単体テストの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,60 +20,60 @@ RSpec.describe User, type: :model do
       it 'ニックネームが入力されていない' do
         @user.nickname = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
       end
       it 'emailが入力されていない' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
       it 'emailが重複している' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include('Email has already been taken')
+        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
       end
       it 'emailが@を含んでいない' do
         @user.email = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Email is invalid')
+        expect(@user.errors.full_messages).to include('Eメールは不正な値です')
       end
       it 'passwordが入力されていない' do
         @user.password = ''
         @user.password_confirmation = '111aaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
       end
       it 'passwordが5文字以下である' do
         @user.password = '11aaa'
         @user.password_confirmation = '11aaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+        expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
       end
       it 'passwordがアルファベットのみである' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+        expect(@user.errors.full_messages).to include('パスワードが無効です。英数字どちらも入力してください')
       end
       it 'passwordが数字のみである' do
         @user.password = '111111'
         @user.password_confirmation = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+        expect(@user.errors.full_messages).to include('パスワードが無効です。英数字どちらも入力してください')
       end
       it 'passwordが全角文字を含んでいる' do
         @user.password = '1a２３４５６７'
         @user.password_confirmation = '1a２３４５６７'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+        expect(@user.errors.full_messages).to include('パスワードが無効です。英数字どちらも入力してください')
       end
       it 'passwordとpassword confirmationが一致しない' do
         @user.password = '111aaa'
         @user.password_confirmation = '211aaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
     end
   end
