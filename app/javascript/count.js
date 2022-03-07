8b1e47e686dd52b4ba030d124625d3d9341889b6
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-07 14:03:22 +0900

    入力されている文字数がカウントされる記述

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,6 +1,9 @@
 function count (){
   const memoContent  = document.getElementById("memo_content");
   memoContent.addEventListener("keyup", () => {
+    const countVal = memoContent.value.length;
+    const charNum  = document.getElementById("char_num");
+    charNum.innerHTML = `${countVal}文字`;
   });
 };
 
